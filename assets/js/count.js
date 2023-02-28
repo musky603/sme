2af984c4bc0d480693c28e5ef8f06c6843b7d090
musky603 <56194336+musky603@users.noreply.github.com>
2023-02-28 08:12:51 +0530

    Update count.js

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,15 +1,20 @@
-// Check if the visit count is already stored in localStorage
-let visitCount = localStorage.getItem('visitCount');
 
+
+<script>
+// Retrieve the visit count from localStorage
+var visitCount = localStorage.getItem('visitCount');
 if (!visitCount) {
-  // If visit count is not stored, set it to 1
-  visitCount = 1;
-  localStorage.setItem('visitCount', visitCount);
-} else {
-  // If visit count is already stored, increment it by 1
-  visitCount = parseInt(visitCount) + 1;
+  visitCount = 0;
+}
+
+// Increment the visit count for new visitors
+if (!sessionStorage.getItem('hasVisited')) {
+  visitCount++;
+  sessionStorage.setItem('hasVisited', true);
   localStorage.setItem('visitCount', visitCount);
 }
 
 // Display the visit count on the page
-document.getElementById('visit-counter').innerHTML = `Views: ${visitCount}.`;
+document.getElementById('visit-count').textContent = 'Total visits: ' + visitCount;
+</script>
+
