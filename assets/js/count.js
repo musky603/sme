7e826aa2e360826966d4fac0847d687dd3cca9cb
musky603 <56194336+musky603@users.noreply.github.com>
2023-03-01 11:17:23 +0530

    Update count.js

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,16 +1,18 @@
-let count = 0; // initial count number
+window.onload = () => {
+  let count = 9; // initial count number
 
-setInterval(() => {
-  count += 3; // increase count by 9
-  document.getElementById("visit-count").innerHTML = `Total visits: ${count}`;
-}, 43200000); // run every 12 hrs (12 * 60*60 * 1000 = 1800000 milliseconds)
-
-setInterval(() => {
-  if (count % 9 === 0) { // check if count is a multiple of 9
+  setInterval(() => {
     count += 3; // increase count by 9
     document.getElementById("visit-count").innerHTML = `Total visits: ${count}`;
-  }
-}, 86400000); // run every 24 hours (24 * 60 * 60 * 1000 = 86400000 milliseconds)
+  }, 43200000); // run every 30 minutes (30 * 60 * 1000 = 1800000 milliseconds)
+
+  setInterval(() => {
+    if (count % 9 === 0) { // check if count is a multiple of 9
+      count += 3; // increase count by 9
+      document.getElementById("visit-count").innerHTML = `Total visits: ${count}`;
+    }
+  }, 86400000); // run every 24 hours (24 * 60 * 60 * 1000 = 86400000 milliseconds)
+}
 
 
 
